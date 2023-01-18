a8684a952ed0efb03cc9541b7cae102e57d36cdc
Jesus Vico <jesusvico@gmail.com>
2023-01-18 20:31:20 +0100

    demo: updated

---
+++ demo/client/index.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws');
+
+setTimeout(() => {
+    let socket = new WebSocket('ws://tma_server:8080');
+
+    // Connection opened
+    socket.addEventListener('open', (event) => {
+        console.log('Server open connection');
+    });
+
+    // Listen for messages
+    socket.addEventListener('message', (event) => {
+        console.log('Message from server:', event.data);
+    });
+
+    // Connection closed
+    socket.addEventListener('close', (event) => {
+        console.log('Server closed connection');
+    });
+}, 5000);
